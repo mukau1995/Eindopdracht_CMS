c3a88b0ffbf556c18810dcec42b1ef0d709e2bf6
Elie Mukau Ambel <s109059@ap.be>
2021-01-12 19:12:56 +0100

    Home page is eindelijk af!!

--- elie-cars/src/components/layout.js
+++ elie-cars/src/components/layout.js
@@ -29,14 +29,15 @@ const Layout = ({ children }) => {
       <div
         style={{
           margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
+          maxWidth: "auto",
+          padding: 0,
         }}
       >
         <main>{children}</main>
         <footer
           style={{
             marginTop: `2rem`,
+            textAlign: "center"
           }}
         >
           © {new Date().getFullYear()}, Elie Cars
--- elie-cars/src/pages/index.js
+++ elie-cars/src/pages/index.js
@@ -3,15 +3,39 @@ import { Link, useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/Layout"
 import SEO from "../components/Seo"
+import {Wrapper, Image, Cars, BottomEdgeDown, BottomEdgeUp} from "./pageStyles/pageStyles"
+import {COLORS} from '../constants'
 
 const IndexPage = () => {
-  const data = useStaticQuery(graphql`
+  const {
+    wpcontent:{
+      page:{
+        homeMeta:{
+          homePageHeaderDescription,
+          homePageHeaderTitle,
+          homeHeaderPicture,
+          homePageFeaturedCars 
+        }
+      }
+    }
+  }= useStaticQuery(graphql`
   query {
     wpcontent{
     page(id: "home", idType: URI) {
       homeMeta {
         homePageHeaderDescription
         homePageHeaderTitle
+        homeHeaderPicture{
+          altText
+          sourceUrl
+          imageFile {
+            childImageSharp {
+              fluid(quality: 100){
+                ...GatsbyImageSharpFluid_withWebp
+              }
+            }
+          }
+        }
         homePageFeaturedCars {
           ... on WPGraphql_Car {
             id
@@ -25,6 +49,14 @@ const IndexPage = () => {
                 fieldGroupName
                 picture1 {
                   altText
+                  sourceUrl
+                  imageFile {
+                    childImageSharp {
+                      fluid(quality: 100, grayscale: true){
+                        ...GatsbyImageSharpFluid_withWebp
+                      }
+                    }
+                  }
                 }
               }
             }
@@ -40,6 +72,34 @@ const IndexPage = () => {
   return(
     <Layout>
     <SEO title="Home" />
+    <Wrapper>
+      <div className="banner">
+        <Image fluid={homeHeaderPicture.imageFile.childImageSharp.fluid} alt={homeHeaderPicture.altText}/>
+        <div className="inner-div">
+        <p className="header-title">{homePageHeaderTitle}</p>
+        <p className="header-description">{homePageHeaderDescription}</p>
+      </div>
+      <BottomEdgeDown color={COLORS.BLACK}/>
+      </div>
+      <div className="description">
+        <p>{homePageHeaderDescription}</p>
+        <BottomEdgeUp color={COLORS.PRIMARY}/>
+      </div>
+      <div className="cars">
+        <h2>Featured Cars</h2>
+        <div className="car-items">
+          {homePageFeaturedCars.map(({carsMeta, slug})=>(
+            <Cars to={`${slug}`}>
+              <Image fluid={carsMeta.pictures.picture1.imageFile.childImageSharp.fluid} altText={carsMeta.make, carsMeta.model}/>
+              <div className="car-info">
+              <p>{carsMeta.make} {carsMeta.model}</p>
+              <p>{carsMeta.hp}</p>
+              </div>
+            </Cars>
+          ))}
+        </div>
+      </div>
+    </Wrapper>
   </Layout>
   )
 }
