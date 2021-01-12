7d1041feaa6cd603bd78d492f2742c0f61eaf38b
Elie Mukau Ambel <s109059@ap.be>
2021-01-12 14:38:37 +0100

    bezig met de homepage

--- elie-cars/src/components/header/Header.js
+++ elie-cars/src/components/header/Header.js
@@ -1,29 +1,41 @@
-import { graphql, Link, useStaticQuery } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
-import {HeaderWrapper} from "./headerStyles/headerStyles"
+import { useStaticQuery, graphql, Link } from "gatsby"
+import PropTypes from "prop-types"
+import Menu from "./Menu"
+import { HeaderWrapper, Image } from "./headerStyles/headerStyles"
 
 const Header = ({ siteTitle }) => {
   const {
-      wpcontent:{menuItems},
- } = useStaticQuery(graphql`
-  query{
-    wpcontent {
-      menuItems {
-        edges {
-          node {
-            label
-            path
+    logo,
+    wpcontent: { menuItems },
+  } = useStaticQuery(graphql`
+    query {
+      logo: file(relativePath: { eq: "elie-cars-logo.png" }) {
+        childImageSharp {
+          fixed(quality: 100, width: 200) {
+            ...GatsbyImageSharpFixed_withWebp
+          }
+        }
+      }
+      wpcontent {
+        menuItems {
+          edges {
+            node {
+              label
+              path
+            }
           }
         }
       }
     }
-  }
   `)
-  console.log(menuItems);
-  return(
+
+  return (
     <HeaderWrapper>
-      
+      <Link to="/">
+      <Image alt="logo elie cars" fixed={logo.childImageSharp.fixed} />
+      </Link>
+      <Menu menuItems={menuItems.edges} />
     </HeaderWrapper>
   )
 }
--- elie-cars/src/pages/index.js
+++ elie-cars/src/pages/index.js
@@ -1,13 +1,48 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/Layout"
 import SEO from "../components/Seo"
 
-const IndexPage = () => (
-  <Layout>
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+  query {
+    wpcontent{
+    page(id: "home", idType: URI) {
+      homeMeta {
+        homePageHeaderDescription
+        homePageHeaderTitle
+        homePageFeaturedCars {
+          ... on WPGraphql_Car {
+            id
+            carsMeta {
+              color
+              hp
+              fieldGroupName
+              make
+              model
+              pictures {
+                fieldGroupName
+                picture1 {
+                  altText
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+  
+  
+  `)
+  return(
+    <Layout>
     <SEO title="Home" />
   </Layout>
-)
+  )
+}
+
 
 export default IndexPage
