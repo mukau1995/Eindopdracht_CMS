0feca66edbd2725cc7f0ca731c0dd6d421ce4e2e
Elie Mukau Ambel <s109059@ap.be>
2021-01-13 01:09:08 +0100

    Bezig met de banner van de cars page

--- elie-cars/src/pages/cars.js
+++ elie-cars/src/pages/cars.js
@@ -7,21 +7,58 @@ import {Wrapper, Image, Cars, BottomEdgeDown, BottomEdgeUp} from "./pageStyles/p
 import {COLORS} from "../constants"
 
 const CarsPage = ()=>{
-    // const {
-    //     wpcontent: {
-    //       page: {
-    //         carsPMeta: { carssPageDescription, bannerPicture },
-    //       },
-    //       cars: { edges: cars },
-    //     },
-    //   } = useStaticQuery(graphql()
+    const {
+        wpcontent:{
+       page:{
+           carsPMeta:{carsPageDescription,
+            carsPageHeaderPictcure},
+       },
+       cars:{edges: cars}    
+        }
+    } = useStaticQuery(graphql`
+    query{
+        wpcontent {
+            page(id: "cars", idType: URI) {
+              carsPMeta {
+                carsPageHeaderPictcure {
+                    sourceUrl
+                    imageFile{
+                        childImageSharp{
+                            fluid(quality:100){
+                                ...GatsbyImageSharpFluid_withWebp
+                            }
+                        }
+                    }
+                  altText
+                }
+                carsPageDescription
+              }
+            }
+            cars {
+                edges {
+                  node {
+                    carsMeta {
+                      color
+                      make
+                      hp
+                      model
+                      year
+                    }
+                    slug
+                  }
+                }
+              }
+        }
+    }
+    `)
     
     return(
         <Layout>
             <SEO title="Cars" />
             <Wrapper carsColor={COLORS.BLACK} descriptionColor={COLORS.SECONDARY}>
                 <div className="banner">
-                   
+                   <Image fluid={carsPageHeaderPictcure.imageFile.childImageSharp.fluid}/>
+                   <BottomEdgeDown color={COLORS.BLACK}/>
                 </div>
             </Wrapper>
         </Layout>
